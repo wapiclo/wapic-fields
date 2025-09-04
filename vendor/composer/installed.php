79c53e4009d33b7a0d6d168b4a1d8dd68db43933
Wapiclo <hello@wapiclo.com>
2025-09-04 17:48:26 +0700

    include vendor to git

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wapiclo/wapic-fields',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '9997a60a21985b2e3e806f0151c0dfacbba4e591',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wapiclo/wapic-fields' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '9997a60a21985b2e3e806f0151c0dfacbba4e591',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yahnis-elsts/plugin-update-checker' => array(
+            'pretty_version' => 'v5.6',
+            'version' => '5.6.0.0',
+            'reference' => 'a2db6871deec989a74e1f90fafc6d58ae526a879',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yahnis-elsts/plugin-update-checker',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
