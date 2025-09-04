405a1b84b96d1cfc22bb035c564497f2659af0d4
Wapiclo <hello@wapiclo.com>
2025-09-04 19:05:24 +0700

    include vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wapiclo/wapic-fields',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '6b2c96c4e6c2be2a772880f1651394e620d74f4c',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wapiclo/wapic-fields' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6b2c96c4e6c2be2a772880f1651394e620d74f4c',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yahnis-elsts/plugin-update-checker' => array(
+            'pretty_version' => 'v5.6',
+            'version' => '5.6.0.0',
+            'reference' => 'a2db6871deec989a74e1f90fafc6d58ae526a879',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yahnis-elsts/plugin-update-checker',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
