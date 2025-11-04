853224348330cad05aa32c3437a5ed03409ec4e8
Wapiclo <hello@wapiclo.com>
2025-11-04 14:42:52 +0700

    Add sample repeater

--- examples/example.php
+++ examples/example.php
@@ -5,3 +5,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 require_once __DIR__ . '/class-example-option.php';
 require_once __DIR__ . '/class-example-meta.php';
 require_once __DIR__ . '/class-example-taxonomy.php';
+require_once __DIR__ . '/class-example-repeater.php';
